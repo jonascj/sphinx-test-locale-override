d13b51e68cd2ba582c73de163909aa678a785fca
Jonas Camillus Jeppesen <jonascj@camillus.dk>
2019-08-13 21:54:10 +0200

    Renamed locale/en/ to locale/da/ and now overriding works as expected

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'da'
+language = 'en'
 locale_dirs = ['locale/']
 gettext_auto_build = True
 gettext_compact = False
